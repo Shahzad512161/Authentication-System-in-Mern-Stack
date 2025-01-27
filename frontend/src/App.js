705d0c1471bce98f364e406daca9b323e2720514
Muhammad Shahzad Diyal <shahzaddiyal786@gmail.com>
2025-01-27 13:24:14 +0500

    private routing is created successfully

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,16 +1,25 @@
 import { Routes, Route, Navigate } from "react-router-dom";
-import Login from "./pages/Login"; // Adjust imports as needed
-import Signup from "./pages/Signup";
-import Main from "./pages/Main"
+import Login from "./pages/Login"; 
+import Signup from "./pages/Signup"; 
+import Main from "./pages/Main"; 
+import { useState } from "react";
+import RefreshHandler from "./pages/RefreshHandler";
 
 function App() {
+  const [isAuthenticated, setIsAuthenticated] = useState(false);
+
+  const PrivateRoute = ({ element }) => {
+    return isAuthenticated ? element : <Navigate to="/login" />;
+  };
+
   return (
     <div className="App">
+      <RefreshHandler setIsAuthenticated={setIsAuthenticated} />
       <Routes>
         <Route path="/" element={<Navigate to="/login" />} />
-        <Route path="/main" element={<Main />} />
         <Route path="/login" element={<Login />} />
         <Route path="/signup" element={<Signup />} />
+        <Route path="/main" element={<PrivateRoute element={<Main />} />} />
       </Routes>
     </div>
   );
--- frontend/src/pages/Main.jsx
+++ frontend/src/pages/Main.jsx
@@ -1,5 +1,7 @@
 import React, { useEffect, useState } from 'react'
 import { useNavigate } from 'react-router-dom';
+import { ToastContainer } from 'react-toastify';
+import { handleSuccess } from '../utils';
 
 const Main = () => {
     const [LoggedInUser , setLoggedInUser] = useState('');
@@ -11,15 +13,40 @@ const Main = () => {
     const handleLogoutClick =(e) =>{
         localStorage.removeItem('token')
         localStorage.removeItem('LoggedInUser')
+        handleSuccess('Logout Successfully')
         setTimeout(()=>{
             navigate('/login')
         },1000)
     }
+
+
+    const fetchProducts = async () => {
+        try {
+            const url = url('http://localhost:8080/product')
+            const headers = {
+                header:{
+                    'Authorization':localStorage.getItem('token')
+                }
+            }
+            const response = await fetch(url,headers);
+            const result = await response.json();
+            console.log(result)
+        } catch (error) {
+            
+        }
+    }
+    
+    useEffect(()=>{
+        fetchProducts()
+    })
   return (
     <div>
         <h1>Welcome , {LoggedInUser}</h1>
         <button onClick={handleLogoutClick}>Logout</button>
+
+        <ToastContainer />
     </div>
+
   )
 }
 
